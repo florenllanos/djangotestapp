2be641be9e8ba9bd1299aacf9d2e17476438c2cf
Floren Llanos <florenllanos@gmail.com>
2015-04-30 22:47:07 +0200

    Work in progress
    Work in progress

--- djangotestapps/settings.py
+++ djangotestapps/settings.py
@@ -94,3 +94,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Templeate dir
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, '/gesevent/templates'),  # gesevent templates
+)
